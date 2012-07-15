c1371137cdcddeac8afde66e365b002e1cdb0a68
Tasos Latsas <tlatsas2000@gmail.com>
2012-07-15 15:33:59 +0300

    initial commit

---
+++ jinja2_highlight/highlight.py
@@ -0,0 +1,59 @@
+# -*- coding: utf8 -*-
+
+import sys
+from jinja2 import nodes
+from jinja2.ext import Extension
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name, guess_lexer
+from pygments.formatters import HtmlFormatter
+from pygments.util import ClassNotFound
+
+class HighlightExtension(Extension):
+    """Highlight code blocks using Pygments
+
+    Example::
+
+        {% highlight 'python' %}
+
+        from fridge import Beer
+
+        pint_glass = Beer()
+        pint_glass.drink()
+
+        {% endhighlight %}
+    """
+    tags = set(['highlight'])
+
+    def parse(self, parser):
+        lineno = parser.stream.next().lineno
+
+        # TODO:
+        # add support to show line numbers
+
+        # extract the language if available
+        if not parser.stream.current.test('block_end'):
+            lang = parser.parse_expression()
+        else:
+            lang = nodes.Const(None)
+
+        # body of the block
+        body = parser.parse_statements(['name:endhighlight'], drop_needle=True)
+
+        return nodes.CallBlock(self.call_method('_highlight', [lang]),
+                               [], [], body).set_lineno(lineno)
+
+    def _highlight(self, lang, caller=None):
+        # highlight code using Pygments
+        body = caller()
+        try:
+            if lang is None:
+                lexer = guess_lexer(body)
+            else:
+                lexer = get_lexer_by_name(lang, stripall=False)
+        except ClassNotFound as e:
+            print(e)
+            sys.exit(1)
+
+        formatter = HtmlFormatter()
+        return highlight(body, lexer, formatter)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+    name='jinja2-highlight',
+    version='0.2',
+    description='Jinja2 extension to highlight source code using Pygments',
+    author='Tasos Latsas',
+    author_email='tlatsas2000@gmail.com',
+    url='https://github.com/tlatsas/jinja2-highlight',
+    license='GNU GPLv3',
+    packages=['jinja2_highlight'],
+)
