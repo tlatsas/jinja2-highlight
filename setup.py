b8a632833464dfecefda5c837b6f35bc1cc79a15
Tasos Latsas <tlatsas@gmx.com>
2014-09-13 20:57:27 +0300

    Bump 0.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup (
     name='jinja2-highlight',
-    version='0.5.1',
+    version='0.6.1',
     description='Jinja2 extension to highlight source code using Pygments',
     keywords = 'syntax highlighting',
     author='Tasos Latsas',
